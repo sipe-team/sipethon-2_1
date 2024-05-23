2db652c46a41e6b9ca2ccd8299a51f281fea12b9
jimin3263 <dkdldsla0000@naver.com>
2024-05-24 01:12:41 +0900

    chore: jdk 버전 변경

--- backend-api/build.gradle.kts
+++ backend-api/build.gradle.kts
@@ -28,7 +28,7 @@ group = "com.sipe"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
@@ -65,7 +65,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "21"
+        jvmTarget = "17"
     }
 }
 
