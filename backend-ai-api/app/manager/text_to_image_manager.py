6c2d70a80543e9fe8d8eabb92b762ecf8f21966d
terry <terry@wrtn.io>
2024-05-25 05:48:13 +0900

    feat: return default image when image generation failed

--- backend-ai-api/app/manager/text_to_image_manager.py
+++ backend-ai-api/app/manager/text_to_image_manager.py
@@ -14,13 +14,15 @@ class Text2ImageManager:
     def generate_image_url(self, data: list):
         qa_manager = QAManager()
         text = qa_manager.summary(data)
-        print(text)
 
-        response = self.client.images.generate(
-            model="dall-e-3",
-            prompt=text,
-            size="1024x1024",
-            quality="standard",
-            n=1,
-        )
-        return response.data[0].url
+        try:
+            response = self.client.images.generate(
+                model="dall-e-3",
+                prompt=text,
+                size="1024x1024",
+                quality="standard",
+                n=1,
+            )
+            return response.data[0].url
+        except:
+            return "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihb60vL8q0Rpbky7mXzFHm1YROhDj3Q3kWw6SO52BHGiXhzFe7ySKI8zjYbpc-yL21zfPM3I5hfdSXZktLeU0XGr4qMuoaOyVUc=w2762-h2328"
