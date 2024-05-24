f380dd7b67bf5d972a4f3b70c688e968703f65e8
som-syom <haim.som@kakaostyle.com>
2024-05-25 04:15:22 +0900

    chore: asset 추가

--- frontend/app/feature/MainContents/index.tsx
+++ frontend/app/feature/MainContents/index.tsx
@@ -1,6 +1,7 @@
 import { ImageContainer } from "~/components/ImageContainer";
 import { container, mainText, subText } from "./style.css";
 import { Button } from "~/components/Button";
+import MainImage from "~/assets/main.png";
 
 const MAIN_TITLE = `취향에 맞는 향수를
 찾기 위한 5번의 대화`;
@@ -8,9 +9,6 @@ const MAIN_TITLE = `취향에 맞는 향수를
 const SUB_TEXT = `환영합니다! 저는 향수 고양이 향고입니다.
 당신이 좋아할 만한 향수를 추천해주고
 그 향수를 뿌린 당신의 모습을 그림으로 그려줄게요!`;
-
-const tempImage =
-  "https://i.pinimg.com/474x/1a/e7/11/1ae711eb21be9ebd2c5d8aaaac0dc00e.jpg";
 
 export const MainContents = () => {
   const handleClick = () => {
@@ -20,7 +18,7 @@ export const MainContents = () => {
     <div className={container}>
       <h1 className={mainText}>{MAIN_TITLE}</h1>
       <h3 className={subText}>{SUB_TEXT}</h3>
-      <ImageContainer src={tempImage} m={"0 auto 60px"} />
+      <ImageContainer src={MainImage} m={"0 auto 60px"} />
       <Button onClick={handleClick}>향고와 대화하기</Button>
     </div>
   );
--- frontend/app/routes/loading.tsx
+++ frontend/app/routes/loading.tsx
@@ -1,8 +1,6 @@
 import { ImageContainer } from "~/components/ImageContainer";
 import * as style from "../styles/common.css";
-
-const tempImage =
-  "https://media.istockphoto.com/id/619079366/photo/let-me-think.jpg?s=612x612&w=0&k=20&c=EC1bomKg5sfnbUdrI1T15k34BHDZvZQwkCPZophe8zw=";
+import LoadingImage from "~/assets/loading.png";
 
 const MAIN_MESSGAE = `좋아할 것 같은 향수
 열심히 가져오는 중`;
@@ -10,7 +8,7 @@ const MAIN_MESSGAE = `좋아할 것 같은 향수
 const LoadingPage = () => {
   return (
     <div className={style.container}>
-      <ImageContainer src={tempImage} alt="thinking" m="150px auto 20px" />
+      <ImageContainer src={LoadingImage} alt="thinking" m="150px auto 20px" />
       <h2 className={style.mainText}>{MAIN_MESSGAE}</h2>
     </div>
   );
